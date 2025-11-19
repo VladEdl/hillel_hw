0ed7653a9213790b113b07e39701f6702b133fac
Vlad <edl.vlad23@gmail.com>
2025-11-19 13:08:38 +0200

    hw_2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 'use strict';
 
-let user = prompt ('What is your name?');
-alert('Hi, ' + user + ', how are you?');
+let user = prompt('What is your name?');
+const ask = confirm('Do you want to see greeting?');
+console.log(ask);
+if (ask)
+{
+    alert('Hi, ' + user + ', how are you?');
+}
+
