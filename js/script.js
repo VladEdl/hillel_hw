c08ed87ad34bf2f4fbd9f8b43c7291fc0725c525
Vlad <edl.vlad23@gmail.com>
2025-11-27 12:15:10 +0200

    hw_6

--- js/script.js
+++ js/script.js
@@ -13,6 +13,6 @@ const price2 = "UAH 999";
 const height = "180cm";
 const broken = "abc123";
 console.log(`"${price1}" -> parseInt: ${parseInt(price1)}, parseFloat: ${parseFloat(price1)}`);
-console.log(`"${price2}" -> parseInt: ${parseInt(price2)}, parseFloat: ${parseFloat(price2)}`);
+console.log(`"${price2}" -> parseInt: ${parseInt(price2)}, parseFloat: ${parseFloat(price2)}`); // Першими стоять букви, а функція читає код до перших символів і потім зупиняється
 console.log(`"${height}" -> parseInt: ${parseInt(height)}, parseFloat: ${parseFloat(height)}`);
 console.log(`"${broken}" -> parseInt: ${parseInt(broken)}, parseFloat: ${parseFloat(broken)}`);
