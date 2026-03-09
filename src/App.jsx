41dfcad3a63c92a35835d3275a5912c2199bb752
Vlad <edl.vlad23@gmail.com>
2026-03-09 21:16:53 +0200

    hw_31

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './card.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app-container">
+                <h1>My Projects</h1>
+                {}
+                <Card />
+            </div>
+        );
+    }
+}
+
+export default App;
